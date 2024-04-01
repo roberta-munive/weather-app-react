64f095d32b0bb3e37e04d07ded22486306152fb4
roberta-munive <114707010+roberta-munive@users.noreply.github.com>
2024-04-01 14:02:21 -0500

    moved container for Bootstrap from index.js to app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <div className="app-container">
+      <div className="app-container container">
         <div className="card weather-watch-card-body">
           {/* search section */}
           <Search />
